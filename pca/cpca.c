dd83415b2086381778e093d9cb2984548816f57b
Gabriel <gabriel.wlazlowski@pw.edu.pl>
2020-04-22 22:06:44 +0200

    Bug fix

--- pca/cpca.c
+++ pca/cpca.c
@@ -451,6 +451,7 @@ int main( int argc , char ** argv )
         MPI_Allreduce( &Nmya, &Ntota, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
         MPI_Allreduce( &Nmyb, &Ntotb, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
         if(ip==0) printf("# INIT3: TOTAL NUMBER OF PARTICLES: SPIN_A=%16.8g SPIN_B=%16.8g\n", Ntota, Ntotb); 
+        if(md.spinsymmetry==1) Ntota = Ntota+Ntotb;
         Effg = 0.6 * Ntota * eF;
         fflush(stdout);
     }
