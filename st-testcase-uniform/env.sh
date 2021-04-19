75370de6eff14620209b867c6dc9b289ae661061
Gabriel <gabriel.wlazlowski@pw.edu.pl>
2021-04-19 17:25:21 +0200

    update of templates

--- st-testcase-uniform/env.sh
+++ st-testcase-uniform/env.sh
@@ -6,4 +6,4 @@ module load openmpi-gcc721-Cuda90/3.1.1
 module load lapack/391
 module load scalapack-gcc721-cuda90-openmpi311/210
 module load elpa-gcc721/202005
-
+module list
--- td-project-template/job.sh
+++ td-project-template/job.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-#PBS -N td-wslda           # <-- SET name 
-#PBS -l nodes=2:ppn=40     # <-- SET number of nodes, ppn=40 to lock the whole node or ppn=8 to allow sharing with others
-#PBS -l walltime=12:00:00  # <-- SET walltime, correlate it with `walltime` input file variable
+##        <-- SET name 
+#PBS -N td-wslda
+##        <-- SET number of nodes, ppn=40 to lock the whole node or ppn=8 to allow sharing with others
+#PBS -l nodes=2:ppn=40
+##        <-- SET memory request, increase it if you encounter problem of type: `PBS: job killed: mem job total exceeded limit`
+#PBS -l mem=128gb
+##        <-- SET walltime, correlate it with `walltime` input file variable
+#PBS -l walltime=12:00:00  
 #PBS -j oe
 #PBS -q long
 
@@ -23,10 +28,11 @@
 ## NOTE: dwarf is not heterogeneous (different nodes have different number of GPUs of a different type),
 ## method of distributing tasks across nodes is provided in predefines.h. 
 
-# execute code
+## execute code
 cd $PBS_O_WORKDIR
 source ./env.sh
-#          <--- SET n = 8*nodes, do NOT modify `-npernode 8`
+
+##         <--- SET n = 8*nodes, do NOT modify `-npernode 8`
 mpirun -n 16 -npernode 8 ./td-wslda-2d input.txt
 
 
