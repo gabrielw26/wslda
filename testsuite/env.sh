88401baa52e1b563e51c1952b25621d34848f9c2
Gabriel <gabriel.wlazlowski@pw.edu.pl>
2021-05-10 11:47:43 +0200

    update of scripts

--- testsuite/env.sh
+++ testsuite/env.sh
@@ -5,3 +5,8 @@ module load openmpi-gcc721-Cuda90/3.1.1
 module load lapack/391
 module load scalapack-gcc721-cuda90-openmpi311/210
 module load elpa-gcc721/202005
+# show modules
+module list
+
+# path to W-SLDA engine
+export WSLDA=/home2/scratch/knfk/cold-atoms/
--- testsuite/testsuiteRun.sh
+++ testsuite/testsuiteRun.sh
@@ -1,7 +1,7 @@
 #!bin/bash
 
-source scl_source enable devtoolset-8 rh-python36
-module load openmpi-gcc721-Cuda90
-module load cuda/9.0
-source env.sh
+# Load env
+source ./env.sh
+
+# run the script
 python3 main.py
