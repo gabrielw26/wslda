5609297145b857e4fe35e8ca22a266f89c1dcfa5
Maksymilian <01133883@pw.edu.pl>
2021-05-11 23:48:40 +0200

    ADDED: short summary, tlist_fast.txt as an input arg

--- testsuite/main.py
+++ testsuite/main.py
@@ -3,21 +3,26 @@ import getpass
 import logging
 import numpy as np
 import os
+import sys
 import time
-import subprocess as sub
 
 #configuration logger in console
 #logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
 #configuration logger in testsuite.log file
-logFilePath=getpass.getuser() + '_testsuite_' + datetime.datetime.now().strftime("%d.%m.%Y_%H.%M") + '.log'
-logging.basicConfig(filename=logFilePath,level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+logFilePath = getpass.getuser() + '_testsuite_' + datetime.datetime.now().strftime("%d.%m.%Y_%H.%M") + '.log'
+logging.basicConfig(filename=logFilePath, level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
 #disable all logger comunicates
 #logging.disable(logging.CRITICAL)
 
 start_time = time.time()
 listOfFolders, listOfTags, listOfMakes, listOfRuns, listOfDiffs, listOfChecks, listOfErrors = [], [], [], [], [], [], []
+
+description_file = 'test.desc'
+if len(sys.argv) == 2: tlist_file = str(sys.argv[1]) #Giventh value for tlist_file variable
+else: tlist_file = 'tlist.txt' #Default variable tlist_file name is "tlist.txt"
+
 
 # Setting default working directory
 def get_global_dir():
@@ -28,16 +33,16 @@ def get_global_dir():
 def count_dirs():
     folder_list = []
     try:
-        file = open('tlist.txt', 'r').read().split('\n') 
-        logging.debug("File \"tlist.txt\" opened.")
+        file = open(tlist_file, 'r').read().split('\n')
+        logging.debug("File \"" + tlist_file + "\" opened.")
     except OSError:
-        logging.critical("\"tlist.txt\" file not found. End of a program.")
+        logging.critical("\"" + tlist_file + "\" file not found. End of a program.")
         exit()
     for line in file:
-        if line.startswith('#') or len(line.strip())==0: continue
+        if line.startswith('#') or len(line.strip()) == 0: continue
         else:
             folder_list.append(line)
-            logging.debug("Directory \"" + str(line) + "\" found in tlist.txt")
+            logging.debug("Directory \"" + str(line) + "\" found in " + "\"" + tlist_file + "\".")
     folder_list = [x for x in folder_list if x != '']
     logging.info("Directories found: " + str(len(folder_list)))
     return folder_list, len(folder_list)
@@ -47,22 +52,22 @@ def check(test_dir):
     if os.path.exists(test_dir):
         return True
     else:
-        logging.warning("Directory \"" +test_dir+ "\" not found!")
+        logging.warning("Directory \"" + test_dir + "\" not found!")
         return False
 
-# Checking if test.desc exists
-def check_desc(dd="test.desc"):
+# Checking if description file exists
+def check_desc(dd=description_file):
     if os.path.exists(dd):
         return True
     else:
         return False
 
-# Counting the amount of tests to perform from each test.desc file
+# Counting the amount of tests to perform from each description file file
 def count_tests():
     try:
-        file = open('test.desc', 'r').read().split('\n')
+        file = open(description_file, 'r').read().split('\n')
     except OSError:
-        logging.error("\"test.desc\" file not found!")
+        logging.error("\"" + description_file + "\" file not found!")
         return -1
     amnt = sum([1 for line in file if line.startswith('tag:')])
     return amnt
@@ -88,9 +93,9 @@ def run_diff_test(output, reference):
     for i in range(len(out_vals)):
         if abs(out_vals[i] - ref_vals[i]) < ref_tols[i]:
             comparedValues.append(True)
-            logging.info("PASS! - Output: "+ str(out_tags[i]) + " = " + str(out_vals[i]) + " Reference: " + str(ref_tags[i]) + " = " + str(ref_vals[i]) + " Epsilon = " + str(ref_tols[i]) + " | (out_val - ref_val) = " + str(out_vals[i] - ref_vals[i]))
+            logging.info("PASS! - Output: " + str(out_tags[i]) + " = " + str(out_vals[i]) + " Reference: " + str(ref_tags[i]) + " = " + str(ref_vals[i]) + " Epsilon = " + str(ref_tols[i]) + " | (out_val - ref_val) = " + str(out_vals[i] - ref_vals[i]))
         else:
-            logging.info("WRONG! - Output: "+ str(out_tags[i]) + " = " + str(out_vals[i]) + " Reference: " + str(ref_tags[i]) + " = " + str(ref_vals[i]) + " Epsilon = " + str(ref_tols[i]) + " | (out_val - ref_val) = " + str(out_vals[i] - ref_vals[i]))
+            logging.info("FAIL! - Output: " + str(out_tags[i]) + " = " + str(out_vals[i]) + " Reference: " + str(ref_tags[i]) + " = " + str(ref_vals[i]) + " Epsilon = " + str(ref_tols[i]) + " | (out_val - ref_val) = " + str(out_vals[i] - ref_vals[i]))
             comparedValues.append(False)
     if all(values is True for values in comparedValues):
         now_check = 'OK'
@@ -98,11 +103,11 @@ def run_diff_test(output, reference):
         now_check = 'FAIL'
     return now_check
 
-# Executing Unix commands given for each test in test.desc file
+# Executing Unix commands given for each test in description file
 def commands():
     tag_test_counter = 0
     tab_checks, tab_makes, tab_runs, tab_diffs = [], [], [], []
-    file = open('test.desc', "r").read().split('\n')
+    file = open(description_file, "r").read().split('\n')
     for line in file:
         if line.startswith('#'): continue
         if line.startswith('tag:'):
@@ -142,7 +147,7 @@ def commands():
             tagg, diff_comm = line.split(': ')
             files_to_compare = diff_comm.split(' ')
             file1, file2 = files_to_compare[0], files_to_compare[1]
-            logging.info("Checking if files \"" + str(file1) +  "\" and \"" + str(file2) + "\" exist.")
+            logging.info("Checking if files \"" + str(file1) + "\" and \"" + str(file2) + "\" exist.")
             if os.path.exists(file1) and os.path.exists(file2):
                 logging.info("Comparing files \"" + str(file1) + "\" and \"" + str(file2) + "\"")
                 check_diff = run_diff_test(file1, file2)
@@ -158,20 +163,48 @@ def commands():
     return tab_makes, tab_runs, tab_checks
 
 # Generating final report
-def runReport(Folder, Tag, Make, Run, Check):
+def runReport(__Folder, __Tag, __Make, __Run, __Check):
+    #Settings of the report output file
+    filepath = S + '/' + getpass.getuser() + '_report_' + datetime.datetime.now().strftime("%d.%m.%Y_%H.%M") + '.txt'
+    if os.path.exists(filepath): os.remove(filepath)
     max_arr = []
-    tmp_arr = [Folder, Tag, Make, Run, Check]
+    tmp_arr = [__Folder, __Tag, __Make, __Run, __Check]
     for i in tmp_arr:
         max_arr.append(len(max(i, key=len)))
-    space = max(max_arr) + 2
-    Folder.insert(0, 'Folder')
-    Tag.insert(0, 'Tag')
-    Make.insert(0, 'Make')
-    Run.insert(0, 'Run')
-    Check.insert(0, 'Check')
-    filepath = S + '/' + getpass.getuser() + '_report_' + datetime.datetime.now().strftime("%d.%m.%Y_%H.%M") + '.txt'
-    arrs = np.transpose([Folder, Tag, Make, Run, Check])
-    np.savetxt(filepath, arrs, '%-{}s'.format(space), '\t')
+    spc = max(max_arr) + 2
+
+    #Summarise total amount of tests, OK-tests and FAIL-tests.
+    __tests = len(__Tag)
+    __oks = 0
+    __fails = 0
+    for i in range(len(__Folder)):
+        if __Make[i] == 'OK' and __Run[i] == 'OK' and __Check[i] == 'OK':
+            __oks += 1
+        else:
+            __fails += 1
+    __arr_title = ['--- Summary ---', '']
+    __arr_test = ['TESTS:', __tests]
+    __arr_ok = ['OK:', __oks]
+    __arr_fail = ['FAIL:', __fails]
+    summs = ([__arr_title, __arr_test, __arr_ok, __arr_fail])
+
+    #Main part of report
+    __Folder.insert(0, 'Folder')
+    __Tag.insert(0, 'Tag')
+    __Make.insert(0, 'Make')
+    __Run.insert(0, 'Run')
+    __Check.insert(0, 'Check')
+    arrs = np.transpose([__Folder, __Tag, __Make, __Run, __Check])
+
+    #Generating output file
+    ff = open(filepath, 'a')
+    np.savetxt(ff, summs, '%-7s', '\t')
+    ff.write('\n')
+    if __fails != 0: ff.write('Some test FAILED. For further information check your .log file.\n\n')
+    else: ff.write('SUCCESS. No FAIL occured!\n\n')
+    np.savetxt(ff, arrs, '%-{}s'.format(spc), '\t')
+    ff.close()
+    return __tests, __oks, __fails
 
 # Cute print to the Terminal window
 def cute_print():
@@ -182,12 +215,11 @@ def cute_print():
         print("".join(word.ljust(col_width) for word in row))
     print('\n')
 
-
 # Main function
 S = get_global_dir()  # Set head directory
 F, N = count_dirs()  # Amount and names of tests to perform
-if N==0:
-    logging.critical("No directories found in \"tlist.txt\". End of a program.")
+if N == 0:
+    logging.critical("No directories found in \"" + tlist_file + "\". End of a program.")
     exit()
 for folder in F:
     os.chdir(S)  # Return to the head directory after each iteration
@@ -196,17 +228,17 @@ for folder in F:
     check_if_dir_exists = check(f)  # Checking if dir with tests exists
     if check_if_dir_exists:
         os.chdir(s + '/{}'.format(f))
-        check_desc_file = check_desc()  # Checking if test.desc file exists
+        check_desc_file = check_desc()  # Checking if description file exists
         if check_desc_file:
-            logging.info("\"test.desc\" opened in \""+ f +"\" directory." )
-            amount = count_tests()  # Counting tags starting with "tag" from test.desc file.
-                                    # It returns the amount of tests in each dir
-            logging.info("Number of tests in \"" + str(f)+"\\test.desc\" : " + str(amount))
-            if amount==0:
-                logging.error("There is no test in \"test.desc\"! Skipping " + f + " directory.")
+            logging.info("\"" + description_file + "\" opened in \"" + f + "\" directory.")
+            amount = count_tests()  # Counting tags starting with "tag" from description file.
+            # Returns the amount of tests in each dir
+            logging.info("Number of tests in \"" + str(f) + "\\" + description_file + "\" : " + str(amount))
+            if amount == 0:
+                logging.error("There is no test in \"" + description_file + "\"! Skipping " + f + " directory.")
                 continue
-            if amount==-1:
-                logging.error("There is no \"test.desc\" file! Skipping " + f + " directory.")
+            if amount == -1:
+                logging.error("There is no \"" + description_file + "\" file! Skipping " + f + " directory.")
                 continue
             for test in range(amount):
                 listOfFolders.append(f)
@@ -215,12 +247,18 @@ for folder in F:
             for r in runs: listOfRuns.append(r)
             for c in checks: listOfChecks.append(c)
         else:
-            logging.warning("\"test.desc\" not found in \"" +f+"\" directory. Skipping \"" +f+"\" directory.")
+            logging.warning("\"" + description_file + "\" not found in \"" + f + "\" directory. Skipping \"" + f + "\" directory.")
     else:
-        logging.warning("\""+ f+ "\" directory not found. Skipping \""+ f+ "\" directory.")
+        logging.warning("\"" + f + "\" directory not found. Skipping \"" + f + "\" directory.")
         continue
 
-runReport(listOfFolders, listOfTags, listOfMakes, listOfRuns, listOfChecks)
+xx, yy, zz = runReport(listOfFolders, listOfTags, listOfMakes, listOfRuns, listOfChecks)
+
+print('\n--- Summary ---')
+print('TESTS: ' + str(xx))
+print('OK: ' + str(yy))
+print('FAIL: ' + str(zz))
+
 cute_print()
 print('Done.')
 print("--- %.8s seconds ---" % (time.time() - start_time))
