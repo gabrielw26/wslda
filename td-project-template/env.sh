42293772d1fb347135d57d1645d05315d7885e2e
Gabriel <gabriel.wlazlowski@pw.edu.pl>
2021-09-01 14:03:27 +0200

    added templates

---
+++ td-project-template/env.sh
@@ -0,0 +1,11 @@
+# 
+# W-SLDA Toolkit
+# Warsaw University of Technology
+#  
+
+# List here modules that need to be loaded before the compilation,
+# or update system variables, examples
+#  
+# module load ...
+# export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:path
+#
