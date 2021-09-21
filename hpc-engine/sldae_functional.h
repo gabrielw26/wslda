9034c399fd7298a2c23436c3244a0e60f0d9fc8f
Antoine Boulet <antoine.boulet@pw.edu.pl>
2021-09-21 11:04:40 +0200

    merging of sldae_*.h files

--- hpc-engine/sldae_functional.h
+++ hpc-engine/sldae_functional.h
@@ -1,36 +1,58 @@
-// ###########################################################################
+/**
+ * W-SLDA Toolkit
+ * Author: Antoine Boulet
+ * Creation date: 2021/09/21
+ * */
+
+#ifndef _SLDAE_FUNCTIONAL_
+#define _SLDAE_FUNCTIONAL_
+
+/**
+  ================================== TOOLS ===================================
+ * */
 // ---------------------------------------------------------------------------
-// Value of dimensionless physical quantity of unitary gas at zero temperature
-// [default: APS[x,y,z] parametrization of the functional]
-//      GSE_UFG = ground state energy
-//      PGE_UFG = pairing gap function
-//      IEM_UFG = inverse effective mass
-//
-// GSE_UFG = 1-16/(3*PI)*atan((5/ 24) / ((6/(35*PI))*(11-2*ln(2))))
-// from APS_X functional, in aggrement with MBPT(2)
-//
-// PGE_UFG and IEM_UFG are adjusted on Gorkov Green's function caluclation of
-// [Phys. Rev. A80, 063612 (2009)] defining the APS[x,y,z] parametrization.
+#define HFB_ORDER 8
+#define FUNCTIONAL_ORDER 2
+// ---------------------------------------------------------------------------
+#define LN2 log(2.)
+#define LN2_2 pow(LN2, 2)
+#define LN2_3 pow(LN2, 3)
+#define LN2_4 pow(LN2, 4)
+#define LN2_5 pow(LN2, 5)
+#define M_PI_SQ pow(M_PI, 2)
+// ---------------------------------------------------------------------------
+#define DDCC_EPSILON 1.0e-32
+// minimal _x to avoide divergences
+// ---------------------------------------------------------------------------
+double rising_factorial (double x, int in);
+double falling_factorial (double x, int in);
+double binomial_coefficient (int in, int ik);
+double exponential_integral (double x);
+double pexp_bell_polynomial (int in, int ik, double * g);
+double xm (int dn, double x, int * ip);
+// ---------------------------------------------------------------------------
+double power_lrule (int dn, double _x, int i, int dk, int * id, double (*f) (int, double, int *));
+double product_lrule (int dn, double _x, int dkf, int dkg, int * idf, int * idg, double (*f) (int, double, int *), double (*g) (int, double, int *));
+double inverse_lrule (int dn, double _x, int dk, int * id, double (*f) (int, double, int *));
+double composed_lrule (int dn, double _x, int dkf, int dkg, int * idf, int * idg, double (*f) (int, double, int *), double (*g) (int, double, int *));
+// ---------------------------------------------------------------------------
+double b_expansion (int dp, double y_x, int * idx);
+double c_expansion (int dp, double y_x, int * idx);
+// ---------------------------------------------------------------------------
+// ###########################################################################
+
+
+// ---------------------------------------------------------------------------
+
+
+/**
+  ================================ FUNCTIONAL ================================
+**/
 // ---------------------------------------------------------------------------
 #define GSE_UFG 0.3582341
 #define PGF_UFG 0.4600000
 #define IEM_UFG 0.8403361
-// ---------------------------------------------------------------------------
-// ###########################################################################
-
-
-
-// ---------------------------------------------------------------------------
-
-
-
-// ###########################################################################
-// ---------------------------------------------------------------------------
-// constants used in APS[x,y,z] parametrization
-// ---------------------------------------------------------------------------
-
-// APS[x,y,z] constants
-// ====================
+// --------------------------------------------------------------------------
 #define U_APS (5. / 24.)
 #define V_APS (U_APS / tan(3. * M_PI / 16. * (1. - GSE_UFG)))
 #define V_OVER_U_APS (V_APS / ((6. / (35. * M_PI)) * (11. - 2. * LN2)))
@@ -40,58 +62,17 @@
 #define X_APS ((9. * M_PI * pow(U_APS, 2)) / (7. * U_APS * V_APS) * ((5. * U_APS * W_APS) / (9. * M_PI * pow(U_APS, 2)) - (pow(U_APS, 2) + pow(V_APS, 2)) / U_APS * (IEM_UFG - 1.)))
 #define A_APS ((5. * U_APS * W_APS - 7. * X_APS * U_APS * V_APS) / (9. * M_PI * pow(U_APS, 2)))
 #define B_APS ((2. * W_APS + 7. * X_APS * V_APS) / (9. * M_PI * pow(U_APS, 2)))
-
-// ---------------------------------------------------------------------------
-// ###########################################################################
-
-
-
-// ---------------------------------------------------------------------------
-
-
-
-// ###########################################################################
-// ---------------------------------------------------------------------------
-// functions of _x used in APS[x,y,z] parametrization
 // ---------------------------------------------------------------------------
 double s_aps (int dn, double _x, int * id);
 double s_aps_2d1 (int dn, double _x, int * id);
 double pairing_bcs (int dn, double _x, int * id);
 double pairing_fit (int dn, double _x, int * id);
 // ---------------------------------------------------------------------------
-// ###########################################################################
-
-
-
-// ---------------------------------------------------------------------------
-
-
-
-// ###########################################################################
-// ---------------------------------------------------------------------------
-// ground state energy, chemical potential, inverse effective mass,
-// and pairing gap as a function of _x = |akF| (can be re-defiend by user)
-// ---------------------------------------------------------------------------
 double ground_state_energy (int dn, double _x, int * id);
 double chemical_potential (int dn, double _x, int * id);
 double inverse_effective_mass (int dn, double _x, int * id);
 double pairing_gap (int dn, double _x, int * id);
 // ---------------------------------------------------------------------------
-// ###########################################################################
-
-
-
-
-// ---------------------------------------------------------------------------
-
-
-
-
-// ###########################################################################
-// ------------------------------------------------------[! DO NOT MODIFY >>>]
-// usefull mathematical tools for calculation            [! DO NOT MODIFY >>>]
-// of HFB parameters a, b, and 1/c                       [! DO NOT MODIFY >>>]
-// ------------------------------------------------------[! DO NOT MODIFY >>>]
 double one_over_a (int dn, double _x, int * id);
 double one_over_h (int dn, double _x, int * id);
 double h_sq (int dn, double _x, int * id);
@@ -103,5 +84,31 @@ double b_series_coefficient (int dn, double _x, int * idp);
 double c_series_coefficient (int dn, double _x, int * idp);
 double b_series (int dn, double _x, int * id);
 double c_series (int dn, double _x, int * id);
-// ------------------------------------------------------[<<< DO NOT MODIFY !]
+// ---------------------------------------------------------------------------
 // ###########################################################################
+
+
+// ---------------------------------------------------------------------------
+
+
+/**
+  ================================ PARAMETERS ================================
+**/
+// ---------------------------------------------------------------------------
+double a_hfb (int dn, double _x, int * id);
+double b_hfb (int dn, double _x, int * id);
+double inverse_c_hfb (int dn, double _x, int * id);
+// ---------------------------------------------------------------------------
+double alpha_parameter (int dn, double _x, int * id);
+double beta_parameter (int dn, double _x, int * id);
+double inverse_gamma_parameter (int dn, double _x, int * id);
+// ---------------------------------------------------------------------------
+double a_functional (double _x, int * id);
+double b_functional (double _x, int * id);
+double c_functional (double _x, int * id);
+// ---------------------------------------------------------------------------
+double pcc_renormalization(double _x, double lmu, int * id);
+// ---------------------------------------------------------------------------
+// ###########################################################################
+
+#endif
