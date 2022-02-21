48091528292bd003d83d9c21cddbc4a9f8a8459b
Gabriel Wlazlowski <gabrielw@daint101.login.cscs.ch>
2022-02-21 15:59:21 +0100

    update of templetes for daint

--- templates/daint/st/env.sh
+++ templates/daint/st/env.sh
@@ -1,6 +1,7 @@
 module load daint-gpu
-module swap PrgEnv-cray PrgEnv-gnu/6.0.9
-module load cudatoolkit/11.2.0_3.39-2.1__gf93aa1c
+module swap PrgEnv-cray PrgEnv-gnu/6.0.10
+# module load cudatoolkit/11.2.0_3.39-2.1__gf93aa1c
+module load cudatoolkit/10.2.89_3.28-2.1__g52c0314
 module load craype-accel-nvidia60
 module load cray-fftw
 export LD_LIBRARY_PATH=/project/pr125/share/elpa-2020.11.001/lib:$LD_LIBRARY_PATH
