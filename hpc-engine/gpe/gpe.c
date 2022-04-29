c91d9d7537bbcf111e7e96af2680ca3d7295fe24
Gabriel <gabriel.wlazlowski@pw.edu.pl>
2022-04-29 17:26:36 +0200

    gw changes

--- hpc-engine/gpe/gpe.c
+++ hpc-engine/gpe/gpe.c
@@ -8,6 +8,8 @@
 #include "pca_utils.h"
 #include "gpe_engine_api.h"
 #include "wslda_reproducibility.h"
+#include "wdata.h"
+
 int wsldapid;
 
 int main( int argc , char ** argv ) 
@@ -79,7 +81,59 @@ int main( int argc , char ** argv )
     // Create empty WDATA set
     // use example:
     // https://gitlab.fizyka.pw.edu.pl/wtools/wdata/-/blob/master/c-examples/example-write.c
+    // create metadata handler
+    wdata_metadata wmd;
 
+    // Lattice
+    wmd.datadim = 3; // 3D data
+    wmd.nx = NX;
+    wmd.ny = NY;
+    wmd.nz = NZ;
+    wmd.dx = DX;
+    wmd.dy = DY;
+    wmd.dz = DZ;
+    strcpy(wmd.prefix, input->outprefix);
+    wmd.t0 = 0.0;
+    wmd.dt = input->dt*input->timesteps;
+    
+    // add variables to data set
+    // for each variable binary file of name `prefix_`varname`.wdat will be created
+    wdata_variable vdensity_a = {"density_a", "real", "none", "wdat"};
+    wdata_add_variable(&wmd, &vdensity_a);
+    wdata_variable vdelta = {"psi", "complex", "none", "wdat"};
+    wdata_add_variable(&wmd, &vdelta);
+    wdata_variable vcurrent_a = {"current_a", "vector", "none", "wdat"};
+    wdata_add_variable(&wmd, &vcurrent_a);
+
+    // add links to data sets
+    // links are alternative names of the same variable
+    wdata_link ldensity_b = {"density_b", "density_a"};
+    wdata_add_link(&wmd, &ldensity_b);
+    wdata_link lcurrent_b = {"current_b", "current_a"};
+    wdata_add_link(&wmd, &lcurrent_b);
+    wdata_link ldelta = {"delta", "psi"};
+    wdata_add_link(&wmd, &ldelta);
+
+    // add constants
+//     wdata_const lconst_eF = {"eF", 0.5, "MeV"};
+//     wdata_add_const(&wmd, &lconst_eF);
+// 
+//     wdata_const lconst_kF = {"kF", 1.0, "1/fm"};
+//     wdata_add_const(&wmd, &lconst_kF);
+
+    // just in case - clear data sets
+    // it removes binary files if they alredy exists
+    wdata_clear_database(&wmd);
+    gpe_get_psi_api(&time, psi);
+    wdata_write_cycle(&wmd, "psi", psi);
+    
+//         gpe_get_density(&time, psi);
+    // outprefix_density.wdat
+//         gpe_get_current(&time, psi);
+    // outprefix_current.wdat
+    wdata_add_cycle(&wmd);
+    wdata_write_metadata_to_file(&wmd, "");
+    
     // TODO
     // if(mode=...)
     while(1)
@@ -104,23 +158,37 @@ int main( int argc , char ** argv )
 
         
         // Add new data to WDATA set
-        // outprefix_psi.wdat        
+        gpe_get_psi_api(&time, psi);
+        wdata_write_cycle(&wmd, "psi", psi);
+        
 //         gpe_get_density(&time, psi);
         // outprefix_density.wdat
 //         gpe_get_current(&time, psi);
         // outprefix_current.wdat
         
-        // add enetry to logger
+        wdata_add_cycle(&wmd);
+        wdata_write_metadata_to_file(&wmd, "");
+        
+        // add entry to logger
+        // na początku kodu utworzyć plik tekstowy outprefix.wlog
+        //kF=referencekF();
+        // przygotować strukture wslda_density
+        // logger(...)
 
-        if(mode==0 && fabs(diff) < input->energyconveps) break;
-        if(mode==1 && time > input->timesteps) break;
+        if(mode==0 && fabs(diff) < input->energyconveps) break; // algorithm converged
+        if(time > dt*input->timesteps*input->measurements) 
+        {
+            if(mode==0) printf("WARNING: ...\n")
+            break; // do not allow to iterate infinitly long
+        }
     }
 
     // close files
     
-    gpe_get_psi_api(&time, psi); //TOREMOVE
-    write_to_binary_file(nxyz, psi); //TOREMOVE
-    write_to_txt_file(nx, ny, nz, psi); //TOREMOVE
+//     gpe_get_psi_api(&time, psi); //TOREMOVE
+//     write_to_binary_file(nxyz, psi); //TOREMOVE
+//     write_to_txt_file(nx, ny, nz, psi); //TOREMOVE
+
     gpe_destroy_engine_api();
     free_host_memory(psi);
 
