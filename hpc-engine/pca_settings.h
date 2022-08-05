44a961e9279af566f028c230b754b6ff43b4edfe
Gabriel <gabriel.wlazlowski@pw.edu.pl>
2022-08-05 13:06:12 +0200

    test update

--- hpc-engine/pca_settings.h
+++ hpc-engine/pca_settings.h
@@ -20,7 +20,7 @@
 // uncomment this if you want to assure that data is copied to the working buffer 
 // before execution of derivative computation
 // it is related to issue encountered for hipfft implementation
-#define DERIVATIVE_COPY_DATA_MODE
+// #define DERIVATIVE_COPY_DATA_MODE
 #else
 
 #error "You need to select WSLDA or TDWSLDA!"
