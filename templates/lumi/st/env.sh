3f4a4577622b34f02d30930b49707030c54f064c
Gabriel Wlazłowski <gabriel.wlazlowski@pw.edu.pl>
2022-11-15 13:01:57 +0100

    template update for lumi, #45

--- templates/lumi/st/env.sh
+++ templates/lumi/st/env.sh
@@ -11,3 +11,5 @@ module load cray-fftw
 module load craype-accel-amd-gfx90a
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/project/project_465000150/share/elpa-libsci/PrgEnv-gnu-8.3.3/LUMI-22.06/rocm-5.1.4/lib
+
+cat $WSLDA/VERSION.h
