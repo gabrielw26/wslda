7ce948cdf586239cb0de3b683455efa2d978fc5b
Gabriel Wlazłowski <gabriel.wlazlowski@pw.edu.pl>
2024-08-11 17:09:56 +0200

    preparation for next release

--- VERSION.h
+++ VERSION.h
@@ -1,2 +1,2 @@
 #define CODE "W-SLDA-TOOLKIT"
-#define VERSION "2024.03.22"
+#define VERSION "2024.08.11"
--- hpc-engine/s3dpca.c
+++ hpc-engine/s3dpca.c
@@ -989,8 +989,10 @@ int main( int argc , char ** argv )
 #else
     elpa_set(handle, "nvidia-gpu", telpa_gpu, &info); if(info!=ELPA_OK) error_msg_mpi_abort(iam, info!=ELPA_OK);
 #endif
+#if ELPA_API>=20241103
     // if(telpa_gpu==1) elpa_set(handle,"use_gpu_id", iam%4, &info); 
     if(telpa_gpu==1) elpa_setup_gpu(handle);
+#endif
 #else
     elpa_set(handle, "gpu", telpa_gpu, &info); if(info!=ELPA_OK) error_msg_mpi_abort(iam, info!=ELPA_OK);
 #endif
